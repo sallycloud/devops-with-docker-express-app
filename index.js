dcbfb6474de7bcdfea9bab98bb13719e5ba69bac
Sally Lockhart <57949400+sallycloud@users.noreply.github.com>
2025-12-31 17:09:56 +0100

    Shows version info

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
+const { version } = require('./package.json')
+const message = `<h1>Hello World! (v${version}</h1>`
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send(message)
 })
 
 const PORT = 8080
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server (v${version}) running on port ${PORT}`)
 })
